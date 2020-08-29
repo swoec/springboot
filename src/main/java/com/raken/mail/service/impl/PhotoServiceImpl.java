c0c26aae166ef68e69c0bc87e30529514228ed94
swoec <swoec.wang@gmail.com>
2020-08-30 02:00:21 +1200

    u

--- src/main/java/com/raken/mail/service/impl/PhotoServiceImpl.java
+++ src/main/java/com/raken/mail/service/impl/PhotoServiceImpl.java
@@ -1,17 +1,17 @@
 package com.raken.mail.service.impl;
-import java.time.Duration;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.raken.mail.model.Photo;
+import com.raken.mail.service.PhotoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import com.raken.mail.service.PhotoService;
+
+import java.time.Duration;
 
 @Service
 public class PhotoServiceImpl implements PhotoService {
@@ -27,13 +27,10 @@ public class PhotoServiceImpl implements PhotoService {
                 .build();
     }
 
-    @Autowired
-    private RestTemplate restTemplate;
-
     @Override
     public Photo getPhoto(int id) throws JsonProcessingException {
 
-        final String uri = "http://jsonplaceholder.typicode.com/photos/"+id;
+        final String uri = "http://jsonplaceholder.typicode.com/photos/" + id;
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         ObjectMapper objectMapper = new ObjectMapper();
